78cd733360f397c561cbbb1c2ee9acdca8d8968c
jimtoledo <jimalistairtoledo@gmail.com>
2024-04-03 17:11:55 -0400

    more maddpg mpe reference testing

--- scripts/maddpg.py
+++ scripts/maddpg.py
@@ -554,13 +554,13 @@ if __name__ == '__main__':
         policy_optimizer_lr = 0.0002,
         value_model_fn = lambda num_obs, nA: FCTQV(num_obs, nA, hidden_dims=(256, 256), device=torch.device("cuda")),
         value_optimizer_lr = 0.0004,
-        exploration_noise_process_fn = lambda: NormalNoiseDecayProcess(init_noise_ratio=0.9, decay_steps=10000, min_noise_ratio=0.1),
-        target_policy_noise_process_fn = lambda: NormalNoiseProcess(exploration_noise_ratio=0.05),
-        target_policy_noise_clip_ratio = 0.1,
+        exploration_noise_process_fn = lambda: NormalNoiseDecayProcess(init_noise_ratio=0.9, decay_steps=50000, min_noise_ratio=0.2),
+        target_policy_noise_process_fn = lambda: NormalNoiseProcess(exploration_noise_ratio=0.1),
+        target_policy_noise_clip_ratio = 0.2,
     ),
     replay_buffer_fn = lambda : PrioritizedReplayBuffer(alpha=0.0, beta0=0.0, beta_rate=1.0))
 
-    episode_returns, best_model, saved_models = maddpg.train(env, gamma=0.95, num_episodes=5000, tau=0.005, batch_size=512, save_models=[1, 50, 100, 500, 1000, 2000, 5000])
+    episode_returns, best_model, saved_models = maddpg.train(env, gamma=0.95, num_episodes=8000, tau=0.01, batch_size=512, save_models=[1, 50, 100, 500, 1000, 2000, 5000, 8000])
     results = {'episode_returns': episode_returns, 'best_model': best_model, 'saved_models': saved_models}
     import pickle
     with open('testfiles/maddpg_reference1.results', 'wb') as file:
