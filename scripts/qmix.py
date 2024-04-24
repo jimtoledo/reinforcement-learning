0085b60386c27b8f8ec492b418643c63d14e2889
jimtoledo <jimalistairtoledo@gmail.com>
2024-04-24 14:17:57 -0400

    initial DQRNN code (not tested)

--- scripts/qmix.py
+++ scripts/qmix.py
@@ -12,7 +12,88 @@ NOTE: if param sharing (generally if agents have the same observation and action
     if different obs space, zero pad obs to the max?
     if different action space, zero pad input action, clip output action vector for each individual agent
 '''
+
+class DQRNN(nn.Module):
+
+    def __init__(self, input_dim, output_dim, 
+                 fc1_hidden_dims = (), fc2_hidden_dims = (), rnn_dims = (64, 64), activation_fc = nn.ReLU, device=torch.device("cpu")):
+
+        super(DQRNN, self).__init__()
+        self.activation_fc = activation_fc
+
+        #Build MLP hidden layers
+        fc1_hidden_layers = nn.ModuleList()
+        for i in range(len(fc1_hidden_dims)-1):
+            fc1_hidden_layers.append(nn.Linear(fc1_hidden_dims[i], fc1_hidden_dims[i+1]))
+            fc1_hidden_layers.append(activation_fc())
+
+        fc2_hidden_layers = nn.ModuleList()
+        for i in range(len(fc2_hidden_dims)-1):
+            fc2_hidden_layers.append(nn.Linear(fc2_hidden_dims[i], fc2_hidden_dims[i+1]))
+            fc2_hidden_layers.append(activation_fc())
+        
+        #MLP 1: if no hidden layers, single linear transform from input dim to RNN input dim with activation
+        self.fc1 = nn.Sequential(
+            nn.Linear(input_dim, fc1_hidden_dims[0]),
+            activation_fc(),
+            *fc1_hidden_layers,
+            nn.Linear(fc1_hidden_dims[-1], rnn_dims[0]),
+            activation_fc()
+        ) if fc1_hidden_dims else nn.Sequential(nn.Linear(input_dim, rnn_dims[0]), activation_fc())
+
+        #MLP 2: if no hidden layers, single linear transform from RNN output dim to (action) output dim
+        self.fc2 = nn.Sequential(
+            nn.Linear(rnn_dims[-1], fc2_hidden_dims[0]),
+            activation_fc(),
+            *fc2_hidden_layers,
+            nn.Linear(fc2_hidden_dims[-1], output_dim)
+        ) if fc2_hidden_dims else nn.Linear(rnn_dims[-1], output_dim)
+
+        #GRU cell - NOTE:batch_first=False by default, input/output shape should be (sequence length, batch size, input size)
+        self.rnn = nn.GRUCell(*rnn_dims)
+
+    #input shape: (sequence length, input size) | (sequence length, batch size, input size)
+    def forward(self, inputs):
+        outs = []
+
+        batch_size = inputs.shape[1] if inputs.ndim == 3 else 0
+        rnn_hidden_dims = self.rnn.weight_hh.shape[1]
+
+        #batch process sequence
+        hx = torch.zeros(batch_size, rnn_hidden_dims) if batch_size else torch.zeros(rnn_hidden_dims) #initial RNN hidden layer
+        for t in range(inputs.shape[0]): #sequence length
+            inputs_t = inputs[t] #shape: (input size) | (batch size, input size)
+            x = self.fc1(inputs_t)
+            hx = self.rnn(x, hx)
+            q = self.fc2(hx) #output shape: (output size) | (batch size, output size)
+            outs.append(q) #store batch output at each timestep while stepping through sequence
+            #.unsqueeze(0)
+        
+        qs = torch.stack(outs) #shape: (sequence length, # actions) | (sequence length, batch size, # actions)
+        return qs
+        #torch.stack(dim=1)
     
+    #input shape: (sequence length, input size) | (sequence length, batch size, input size)
+    def select_action(self, inputs):
+        #return current action to be taken (end of sequence)
+        #shape: (# actions) | (batch size, # actions)
+        batch_size = inputs.shape[1] if inputs.ndim == 3 else 0
+        rnn_hidden_dims = self.rnn.weight_hh.shape[1]
+
+        #batch process sequence
+        hx = torch.zeros(batch_size, rnn_hidden_dims) if batch_size else torch.zeros(rnn_hidden_dims) #initial RNN hidden layer
+        for t in range(inputs.shape[0]): #sequence length
+            inputs_t = inputs[t] #shape: (input size) | (batch size, input size)
+            x = self.fc1(inputs_t)
+            hx = self.rnn(x, hx)
+        
+        q = self.fc2(hx) #shape: (sequence length, # actions) | (sequence length, batch size, # actions)
+
+        #return discrete (one-hot) action
+        #TODO: maybe convert tensor output to numpy?
+        return q.detach().max(-1, keepdim=True) #output shape: (1) | (batch size, 1)
+
+
 #Deep Q Recurrent Neural Network
 class MultiAgentDQRNN(nn.Module):
 
